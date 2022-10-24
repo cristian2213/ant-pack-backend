fe17f0eec673e7323417cecc71c378d1167b7f35
Cristian Johany <crislm1322@gmail.com>
2022-10-24 10:21:52 -0500

    fix: folder useless deleted

--- src/test-users/dto/update-test-user.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateTestUserDto } from './create-test-user.dto';
-
-export class UpdateTestUserDto extends PartialType(CreateTestUserDto) {}
--- src/test-users/test-users.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { TestUsersService } from './test-users.service';
-import { TestUsersController } from './test-users.controller';
-
-@Module({
-  controllers: [TestUsersController],
-  providers: [TestUsersService]
-})
-export class TestUsersModule {}
